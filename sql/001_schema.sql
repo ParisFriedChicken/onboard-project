0914dc5e08c968857506c038599db81002a4d641
Sebastien <Sebastien@SebAudio>
2025-10-14 20:38:49 +0200

    Database

--- sql/001_schema.sql
+++ sql/001_schema.sql
@@ -1,49 +1,6 @@
---
--- PostgreSQL database dump
---
-
-\restrict vuHBhCn4DWKiTyOPYuElKYJJhcTWjVy0rdNz8RClGsvEafX3G7fvPKwHNTymm31
-
--- Dumped from database version 18.0
--- Dumped by pg_dump version 18.0
-
--- Started on 2025-10-13 22:02:32
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- TOC entry 4 (class 2615 OID 2200)
--- Name: public; Type: SCHEMA; Schema: -; Owner: pg_database_owner
---
-
-CREATE SCHEMA public;
-
-
-ALTER SCHEMA public OWNER TO pg_database_owner;
-
---
--- TOC entry 4935 (class 0 OID 0)
--- Dependencies: 4
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: pg_database_owner
---
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
+DROP TABLE IF EXISTS game CASCADE;
+DROP TABLE IF EXISTS participation CASCADE;
+DROP TABLE IF EXISTS player CASCADE;
 --
 -- TOC entry 221 (class 1259 OID 24576)
 -- Name: game; Type: TABLE; Schema: public; Owner: postgres
@@ -163,23 +120,6 @@ COPY public.participation (id, game_id, player_id, created_at, no_flake, updated
 
 
 --
--- TOC entry 4925 (class 0 OID 16414)
--- Dependencies: 220
--- Data for Name: player; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.player (id, city, created_at, email, full_name, password, updated_at) FROM stdin;
-107	\N	2025-10-12 21:14:15.371	eg2@gmail.com	Emmanuel Granjean	$2a$10$UMSUC.Wwm/cdxusincXjfOqIfmyETKIjNIjOLLB8jq8pWFbEEMiyW	2025-10-12 21:14:15.371
-153	\N	2025-10-12 21:18:04.805	fg@gmail.com	Fabrice Granjean	$2a$10$U5pNtk7ObkigF6IlYg8fmOxbtb.GxjAVHXROdiKo4MWV3S25frvDi	2025-10-12 21:18:04.805
-154	\N	2025-10-12 21:18:48.598	dr@gmail.com	Daniel Rutin	$2a$10$mjxjFFX.PPr1CzQwiSU0p.SQrf/efu8LgpWpT4mSQIa.5sECmHvku	2025-10-12 21:18:48.598
-155	\N	2025-10-12 21:19:33.224	jd@gmail.com	Jaqueline Daumier	$2a$10$2D8DkgzMYZKl/S5M3npAZewimGYMlk4GZaM1XKSQYdRXVAk7GkZ/2	2025-10-12 21:19:33.224
-157	\N	2025-10-12 21:20:20.193	dp@gmail.com	David Palmer	$2a$10$gOSkzXLXxb11JzMjFmqQuuxxWs.fSUMIydVXsxRZ9phXZPQwnNo4q	2025-10-12 21:20:20.193
-202	\N	2025-10-12 21:22:16.469	mr@gmail.com	Michelle Rutin	$2a$10$ofVBLFjJkEfelLT66nBLK.8DqkyLSL5WeY6sAe5qhAryeiWcxPNMa	2025-10-12 21:22:16.469
-252	New York	2025-10-12 21:24:16.496	sp@gmail.com	Stanislas Pralinos	$2a$10$8KCN.dy64gZHQOU/qbJRNObBEG.TeH3ysoNGkCaCZ4AC0VdQtItRO	2025-10-12 21:24:16.496
-\.
-
-
---
 -- TOC entry 4936 (class 0 OID 0)
 -- Dependencies: 222
 -- Name: game_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
@@ -277,12 +217,4 @@ ALTER TABLE ONLY public.game
 ALTER TABLE ONLY public.participation
     ADD CONSTRAINT fklqo9f5gvkcwq0alysxg6wxlxm FOREIGN KEY (game_id) REFERENCES public.game(id);
 
-
--- Completed on 2025-10-13 22:02:33
-
---
--- PostgreSQL database dump complete
---
-
-\unrestrict vuHBhCn4DWKiTyOPYuElKYJJhcTWjVy0rdNz8RClGsvEafX3G7fvPKwHNTymm31
 
